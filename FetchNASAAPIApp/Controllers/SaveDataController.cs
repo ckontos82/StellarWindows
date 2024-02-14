f7d8ad6bbe7a34dcc575d0a63b5feeeb6bfca957
ckontos <har.kontos@hotmail.com>
2024-02-14 21:46:09 +0200

    Added a simple implementation for saving data to local db. To be updated.

---
+++ FetchNASAAPIApp/Controllers/SaveDataController.cs
@@ -0,0 +1,30 @@
+﻿using FetchNASAAPIApp.Database;
+using FetchNASAAPIApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FetchNASAAPIApp.Controllers
+{
+    public class SaveDataController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SaveDataController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Picture picture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Pictures.Add(picture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index), "NASA");
+            }
+
+            return View(picture);
+        }
+    }
+}
