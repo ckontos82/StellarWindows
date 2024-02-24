5d366d1363f66b9a9117eac13fbc2f7442f35b84
ckontos <har.kontos@hotmail.com>
2024-02-24 16:00:24 +0200

    removing unnecessary comments

--- FetchNASAAPIApp/Database/ApplicationDbContext.cs
+++ FetchNASAAPIApp/Database/ApplicationDbContext.cs
@@ -16,8 +16,8 @@ namespace FetchNASAAPIApp.Database
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Picture>()
-            .HasIndex(p => p.Date) // Specifies the property to index
-            .IsUnique(); // Enforces that the index is unique
+            .HasIndex(p => p.Date) 
+            .IsUnique();
         }
     }
 }
